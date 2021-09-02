15c2b93c08fa12d523cca2db0938fc63e77d9d33
SyedHasibRahman <syedhasib01@gmail.com>
2021-09-02 19:08:21 +0600

    Color changed

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
+// Spiner function 
+const toggleSpinner = displayStyle => {
+    document.getElementById('spinner').style.display = displayStyle;
+}
 const searchBook = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    // console.log(searchText);
     searchField.value = '';
 
+    toggleSpinner('block');
     const url = `http://openlibrary.org/search.json?q=${searchText}`;
-    // console.log(url);
     fetch(url)
         .then(res => res.json())
         .then(data => displaySearchResult(data.docs));
@@ -17,23 +20,33 @@ const displaySearchResult = books => {
     // Count total
     const total = books.length;
     const totalCount = document.getElementById('total-count');
-    totalCount.innerHTML = `<h3>Total ${total} results founded</h3>`;
-    // main div to display results 
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
+    // show limited post 
     if (total !== 0) {
-        books.forEach(book => {
-            console.log(book);
+        books.slice(0, 30).forEach(book => {
+            totalCount.innerHTML = `
+                <h3>Showing ${books.slice(0, 30).length} of Total ${total} results</h3>    
+                <hr>
+                <hr>
+            `;
+            let coverUrl;
+            // validation check for cover image 
+            if (book.cover_i !== undefined) {
+                coverUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+            } else {
+                coverUrl = 'img/NoImageFound.png';
+            }
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
-            <div onclick="loadBookDetail( )" class="col"> 
+            <div class="col shadow-lg p-3"> 
                 <div class="card border border-success border-2">
-                    <img height="250px" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="..."> 
+                    <img height="250px" src="${coverUrl}" class="card-img-top" alt="..."> 
                     <div class="card-body">
                     <h3 class="card-title">${book.title}</h3>
                     <p class="card-text fw-bold">Author: <span class="text-success"> ${book.author_name} </span></p>
-                    <p class="card-text"> Publisher: ${book.publisher} </p>
+                    <p class="card-text"> Publisher: ${book?.publisher} </p>
                     <p class="card-text"><small class="text-muted"> First publishing year: ${book.first_publish_year}</small></p>
                   </div>
                 </div>
@@ -44,7 +57,11 @@ const displaySearchResult = books => {
         });
     }
     else {
-        totalCount.innerHTML = `<h3>Please type a valid name</h3>`;
+        totalCount.innerHTML = `
+        <h3>No results found!</h3>    
+        <hr>
+        <hr>
+        `;
     }
 
 }
